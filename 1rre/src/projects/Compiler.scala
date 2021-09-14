e974fd978c1260554adfdd94104e67d187a6bdbb
Timothy Moores <info@tmoor.es>
2021-09-14 17:52:15 +0100

    Added Pong

--- 1rre/src/projects/Compiler.scala
+++ 1rre/src/projects/Compiler.scala
@@ -2,7 +2,47 @@ package projects
 import org.scalajs.dom.{html, raw}
 import util.Random
 
-case class Compiler(canvas: html.Canvas) extends Project("Compiler", 100d) {
+case class Compiler(canvas: html.Canvas) extends Project("C Compiler", 100d) {
+  final val StatusBarHeight = Height * 0.15
+  final val StatusBarColour = "#E5E5E5"
+  final val TitleColour = "#1C1F22"
+  final val TitleX = 5
+  final val TitleY = StatusBarHeight / 2
+  final val TitleFont = "16px 'Lexend Exa', sans-serif"
+  final val CloseColour = "#E04747"
+  final val CloseRadius = StatusBarHeight * 3 / 8d
+  final val CloseX = Width - StatusBarHeight / 2
+  final val CloseY = StatusBarHeight / 2d
+  final val CrossColour = "#E5E5E5"
+  final val CrossLine1X1 = CloseX - CloseRadius / 2d
+  final val CrossLine1Y1 = CloseY - CloseRadius / 2d
+  final val CrossLine1X2 = CloseX + CloseRadius / 2d
+  final val CrossLine1Y2 = CloseY + CloseRadius / 2d
+  final val CrossLine2X1 = CloseX - CloseRadius / 2d
+  final val CrossLine2Y1 = CloseY + CloseRadius / 2d
+  final val CrossLine2X2 = CloseX + CloseRadius / 2d
+  final val CrossLine2Y2 = CloseY - CloseRadius / 2d
+
+  def drawStatusBar(): Unit = {
+    context.beginPath()
+    context.fillStyle = StatusBarColour
+    context.fillRect(0, 0, Width, StatusBarHeight)
+    context.fillStyle = TitleColour
+    context.font = TitleFont
+    context.textBaseline = "middle"
+    context.fillText("C Compiler", TitleX, TitleY)
+    context.fillStyle = CloseColour
+    context.arc(CloseX, CloseY, CloseRadius, 0, 2 * math.Pi)
+    context.fill()
+    context.strokeStyle = CrossColour
+    context.lineWidth = 2
+    context.beginPath()
+    context.moveTo(CrossLine1X1, CrossLine1Y1)
+    context.lineTo(CrossLine1X2, CrossLine1Y2)
+    context.moveTo(CrossLine2X1, CrossLine2Y1)
+    context.lineTo(CrossLine2X2, CrossLine2Y2)
+    context.stroke()
+  }
   final val TerminalBackground = "#1C1F22"
   final val TerminalFont = "12px 'Overpass Mono', monospace"
   final val TerminalTextX = 5
@@ -13,15 +53,16 @@ case class Compiler(canvas: html.Canvas) extends Project("Compiler", 100d) {
   def useAltTextColour = Random.nextDouble > 0.825
   def terminalTextColour = if (useAltTextColour) "#E04747" else "#007F0E"
 
-  val lines = Array.tabulate(NRows)(_ => (Vector.fill(NCols)(' '), TerminalBackground))
+  val lines =
+    Array.tabulate(NRows)(_ => (Vector.fill(NCols)(' '), TerminalBackground))
   private def updateLines(): Unit = {
     for (i <- lines.indices.tail) {
-      lines(i-1) = lines(i)
+      lines(i - 1) = lines(i)
     }
-    val nChars = Random.between(0,NCols+1)
+    val nChars = Random.between(0, NCols + 1)
     val nSpaces = NCols - nChars
     val chars = Random.alphanumeric.take(nChars).toVector
-    lines(NRows-1) = (chars ++ Vector.fill(nSpaces)(' '), terminalTextColour)
+    lines(NRows - 1) = (chars ++ Vector.fill(nSpaces)(' '), terminalTextColour)
   }
 
   def render(): Unit = {
@@ -32,7 +73,11 @@ case class Compiler(canvas: html.Canvas) extends Project("Compiler", 100d) {
     context.font = TerminalFont
     for (i <- 0 until NRows) {
       context.fillStyle = lines(i)._2
-      context.fillText(lines(i)._1.mkString, TerminalTextX, TerminalTextY+12*i)
+      context.fillText(
+        lines(i)._1.mkString,
+        TerminalTextX,
+        TerminalTextY + 12 * i
+      )
     }
 
   }
--- 1rre/src/projects/Pong.scala
+++ 1rre/src/projects/Pong.scala
@@ -3,8 +3,160 @@ package projects
 import org.scalajs.dom.{html, raw}
 import util.Random
 
-case class Pong(canvas: html.Canvas) extends Project("Pong", 100d) {
-  def render(): Unit = {
-    
+case class Pong(canvas: html.Canvas) extends Project("Pong", 25d) {
+  final val BackgroundColour = "#000000"
+  final val PaddleWidth = 10
+  final val PaddleLength = 60
+  final val PaddleColour = "#E5E5E5"
+  final val PaddleMin = 30
+  final val PaddleMax = 210 - PaddleLength
+  final val BounceScale = 1d/28
+
+
+  abstract class Paddle {
+    def x: Double
+    def y: Double
+    def w: Double
+    def h: Double
+    def update(): Unit
+    def draw(): Unit = {
+      update()
+      context.fillStyle = PaddleColour
+      context.fillRect(x, y, w, h)
+    }
   }
+
+  case class HPaddle(y: Double) extends Paddle {
+    var x = (Width - PaddleLength) / 2d
+    def maxMove: Double = {
+      val dx = (Ball.y - y).abs
+      math.max(0.5, math.min((240 - dx) / 60d, math.hypot(Ball.dx, Ball.dy)))
+    }
+    def update(): Unit = {
+      val target = Ball.x - PaddleLength/2d
+      val max = math.min(PaddleMax, x + maxMove)
+      val min = math.max(PaddleMin, x - maxMove)
+      x = math.min(max, math.max(min, target))
+    }
+    def w = PaddleLength
+    def h = PaddleWidth
+  }
+  case class VPaddle(x: Double) extends Paddle {
+    var y = (Height - PaddleLength) / 2d
+    def maxMove: Double = {
+      val dy = (Ball.x - x).abs
+      math.max(0.5, math.min((240 - dy) / 60d, math.hypot(Ball.dx, Ball.dy)))
+    }
+    def update(): Unit = {
+      val target = Ball.y - PaddleLength/2d
+      val max = math.min(PaddleMax, y + maxMove)
+      val min = math.max(PaddleMin, y - maxMove)
+      y = math.min(max, math.max(min, target))
+    }
+    def w = PaddleWidth
+    def h = PaddleLength
+  }
+  
+  val paddleH1 = new HPaddle(5)
+  val paddleH2 = new HPaddle(Width - 5 - PaddleWidth)
+  val paddleV1 = new VPaddle(5)
+  val paddleV2 = new VPaddle(Height - 5 - PaddleWidth)
+
+  object Ball {
+    final val Radius = 5
+    final val Colour = "#E5E5E5"
+    final val Speed = 100 / refreshRate
+    def initialise(): Unit = {
+      dx = Random.nextDouble() - 0.5
+      dy = Random.nextDouble() - 0.5
+      val hypot = math.hypot(dx, dy)
+      dx *= Speed / hypot
+      dy *= Speed / hypot
+      x = Width / 2d
+      y = Height / 2d
+    }
+
+    var dx = 0d
+    var dy = 0d
+    var x = Width / 2d
+    var y = Height / 2d
+    initialise()
+
+    def bounceX(variant: Double): Unit = {
+      val diff = math.pow(variant * BounceScale, 5)
+      dy += diff * dy.sign
+      dx *= -1
+    }
+    def bounceY(variant: Double): Unit = {
+      val diff = math.pow(variant * BounceScale, 5)
+      dx += diff * dx.sign
+      dy *= -1
+    }
+
+    def willBounceV1: Boolean = (
+      x > 5 + Ball.dx + PaddleWidth + Radius &&
+        x < 5 + PaddleWidth + Radius &&
+        y + Radius >= paddleV1.y &&
+        y - Radius <= paddleV1.y + PaddleLength
+    )
+    def willBounceV2: Boolean = (
+      x < 235 + Ball.dx - PaddleWidth - Radius &&
+        x > 235 - PaddleWidth - Radius &&
+        y + Radius >= paddleV2.y &&
+        y - Radius <= paddleV2.y + PaddleLength
+    )
+    def willBounceH1: Boolean = (
+      y > 5 + Ball.dy + PaddleWidth + Radius &&
+        y < 5 + PaddleWidth + Radius &&
+        x + Radius >= paddleH1.x && 
+        x - Radius <= paddleH1.x + PaddleLength
+    )
+    def willBounceH2: Boolean = (
+      y < 235 + Ball.dy - PaddleWidth - Radius &&
+        y > 235 - PaddleWidth - Radius &&
+        x + Radius >= paddleH2.x && 
+        x - Radius <= paddleH2.x + PaddleLength
+    )
+
+    def draw(): Unit = {
+      context.beginPath()
+      context.fillStyle = Colour
+      context.arc(x, y, Radius, 0, 2 * math.Pi)
+      context.fill()
+      x += dx
+      y += dy
+      if (willBounceV1)
+        bounceX(paddleV1.x+PaddleLength/2 - Ball.x)
+      if (willBounceV2)
+        bounceX(paddleV2.x+PaddleLength/2 - Ball.x)
+      if (willBounceH1)
+        bounceY(paddleH1.y+PaddleLength/2 - Ball.y)
+      if (willBounceH2)
+        bounceY(paddleH2.y+PaddleLength/2 - Ball.y)
+      if (x < 0 || y < 0 || x > Width || y > Height)
+        initialise()
+    }
+  }
+
+  def drawPaddles(): Unit = {
+    paddleH1.draw()
+    paddleH2.draw()
+    paddleV1.draw()
+    paddleV2.draw()
+  }
+
+  def drawBackground(): Unit = {
+    context.fillStyle = BackgroundColour
+    context.fillRect(0, 0, Width, Height)
+  }
+
+  def drawBall(): Unit = {}
+
+  def render(): Unit = {
+    drawBackground()
+    drawPaddles()
+    Ball.draw()
+  }
+
+  render()
 }
